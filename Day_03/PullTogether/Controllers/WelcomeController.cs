a0fcc73c2b9ce9a7f233fec58067a21379bcdbcd
Nihil <35506793+Nihilnia@users.noreply.github.com>
2022-12-07 03:00:14 +0300

    LDR_notCompleted

---
+++ Day_03/PullTogether/Controllers/WelcomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PullTogether.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LoginRegister()
+        {
+            return View();
+        }
+
+
+        
+
+    }
+}
